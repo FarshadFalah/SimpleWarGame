b1d5ebdfc839a4516955b820a56095b704e8add7
Farshad Fallah <farshad.ftf@gmail.com>
2020-09-28 11:37:41 +0330

    Adding JavaDoc to ScannerWrapper

--- src/control/wrappers/ScannerWrapper.java
+++ src/control/wrappers/ScannerWrapper.java
@@ -1,12 +1,19 @@
 package control.wrappers;
 
 import java.util.Scanner;
-
+/*
+**** @author Farshad Fallah
+**** @version 1
+**** this Class wraps Scanner Class
+ */
 public class ScannerWrapper {
     private static final Scanner scanner= new Scanner(System.in);
 
     private ScannerWrapper(){}
 
+    /*
+    @return Integer get a Integer from user Console and return it
+     */
     public static Integer nextInt(){
         return scanner.nextInt();
     }
